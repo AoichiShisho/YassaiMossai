095b08ed7680f423f103c82bfa9c1069c892f90c
中村蒼 <sonakamura@MacBook-Air.local>
2023-10-23 23:39:16 +0800

    fix: can't plant if dirt has a plant

--- Assets/Scripts/PlayerScripts/PlayerPlant.cs
+++ Assets/Scripts/PlayerScripts/PlayerPlant.cs
@@ -8,6 +8,8 @@ public class PlayerPlant : MonoBehaviour
     private bool isOnPlowedDirt = false;
     private Transform currentDirtTransform;
 
+    private bool isPlanting = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +19,12 @@ public class PlayerPlant : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isOnPlowedDirt && Input.GetKeyDown(KeyCode.R)) {
+        if (isOnPlowedDirt && Input.GetKeyDown(KeyCode.R) && !isPlanting) {
+            isPlanting = true;
             PlantCabbage();
+        } else if (isOnPlowedDirt && Input.GetKeyDown(KeyCode.R) && isPlanting) {
+            // あとで変更、植えられなかった時のUIを考える
+            Debug.Log("Can't plant, dirt is occupied.");
         }
     }
 
@@ -31,7 +37,6 @@ public class PlayerPlant : MonoBehaviour
         if (other.CompareTag("Plowed")) {
             isOnPlowedDirt = true;
             currentDirtTransform = other.transform;
-            Debug.Log("Detected Plowed Dirt.");
         }
     }
 
