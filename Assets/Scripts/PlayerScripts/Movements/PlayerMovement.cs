ff7d3cb1121be6779ca0ebd32dbc4b865e70e4a6
中村蒼 <sonakamura@MacBook-Air.local>
2023-11-07 20:25:54 +0900

    clean: cleaned update method in PlayerMovement

--- Assets/Scripts/PlayerScripts/Movements/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/Movements/PlayerMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 3.0f;
+    private float walkSpeed = 3.0f;
+    private float runSpeed = 6.0f;
     private float rotationSpeed = 200.0f;
     private Animator animator;
     private float movementInput;
     private float rotationInput;
+    private bool isRunning;
 
     void Start()
     {
@@ -17,20 +19,35 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movementInput = Input.GetAxis("Vertical");
-        rotationInput = Input.GetAxis("Horizontal");
+        GetInput();
+
         Move();
         Turn();
+
+        UpdateAnimation();
+    }
+
+    void GetInput()
+    {
+        movementInput = Input.GetAxis("Vertical");
+        rotationInput = Input.GetAxis("Horizontal");
+        isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
     }
 
     void Move()
     {
-        transform.Translate(Vector3.forward * movementInput * speed * Time.deltaTime);
-        animator.SetFloat("Speed", Mathf.Abs(movementInput));
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+        transform.Translate(Vector3.forward * movementInput * currentSpeed * Time.deltaTime);
     }
 
     void Turn()
     {
         transform.Rotate(Vector3.up, rotationInput * rotationSpeed * Time.deltaTime);
+    }
+
+    void UpdateAnimation()
+    {
+        float speedFactor = isRunning ? 2.0f : 1.0f;
+        animator.SetFloat("Speed", Mathf.Abs(movementInput) * speedFactor);
     }
 }
