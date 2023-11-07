ffedbef0a9ae31c91168a1ee5955817aef915457
中村蒼 <sonakamura@net40-dhcp176.sfc.keio.ac.jp>
2023-11-07 10:10:40 +0900

    add: player now finds vegetables with OverlapSphere

--- Assets/Scripts/PlayerScripts/PlayerPlantCollector.cs
+++ Assets/Scripts/PlayerScripts/PlayerPlantCollector.cs
@@ -7,25 +7,39 @@ public class PlayerPlantCollector : MonoBehaviour
 {
     public Text cabbageText;
     private int cabbageCount = 0;
+    private PlayerItemSelector itemSelector;
+    private float interactionRange = 5.0f;
+
+    [SerializeField]
+    private LayerMask interactionLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemSelector = GetComponent<PlayerItemSelector>();
+        if (itemSelector == null) {
+            Debug.LogError("PlayerPlantCollector: itemSelector is null");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PickupCabbage();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void PickupCabbage() 
     {
-        if (collision.gameObject.tag == "Plant") {
-            if (Vector3.Distance(transform.position, collision.transform.position) < 1f) {
-                cabbageCount++;
-                UpdateCabbageText();
+        if (Input.GetKeyDown(KeyCode.Space) && itemSelector.SelectedItemIndex == 0) {
+            Debug.Log("PlayerPlantCollector: space pressed");
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, interactionRange, interactionLayer);
+            foreach (var hitCollider in hitColliders) {
+                if (hitCollider.gameObject.tag == "RipeCabbage") {
+                    Debug.Log("Found Ripe Cabbage");
+                    cabbageCount++;
+                    UpdateCabbageText();
+                    break;
+                }
             }
         }
     }
@@ -33,7 +47,7 @@ public class PlayerPlantCollector : MonoBehaviour
     private void UpdateCabbageText()
     {
         if (cabbageText != null) {
-            cabbageText.text = "Cabbage: " + cabbageCount;
+            cabbageText.text = cabbageCount.ToString();
         }
     }
 }
