81c05b70e3f0ae4f5f9a12660121bd580cd0dd34
AoichiShisho <sonakamura.aoichi@gmail.com>
2024-01-09 00:56:54 +0900

    fix - orders now have abstract classes

--- Assets/Scripts/OrderScripts/OrderLarge.cs
+++ Assets/Scripts/OrderScripts/OrderLarge.cs
@@ -3,93 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OrderLarge : MonoBehaviour, IOrder
+public class OrderLarge : OrderBase
 {
-    private List<VegetableType> vegetables;
+    protected override float timeLimit => 40f;
+
     public List<Image> vegetableImages;
     public Sprite tomatoSprite;
     public Sprite cabbageSprite;
 
-    private float timer;
-    private float timeLimit = 40f;
-
-    public Image timeBar;
-
-    void Start()
-    {
-        timer = timeLimit;
-    }
-
-    void Update()
-    {
-        timer -= Time.deltaTime;
-
-        UpdateTimeBar();
-        if (timer <= 0) {
-            DestroyOrder();
-        }
-    }
-
-    private void UpdateTimeBar() 
-    {
-        if (timeBar != null) {
-            timeBar.fillAmount = timer / timeLimit;
-            timeBar.color = GetColorByTime(timer / timeLimit);
-        }
-    }
-
-    private Color GetColorByTime(float timeRatio)
-    {
-        if (timeRatio > 0.65f) {
-            return Color.Lerp(Color.yellow, Color.green, (timeRatio - 0.65f) * 2);
-        } else {
-            return Color.Lerp(Color.red, Color.yellow, timeRatio / 0.65f);
-        }
-    }
-
-    private void DestroyOrder() 
-    {
-        if (transform.parent != null) {
-            var parentGroup = transform.parent.GetComponent<HorizontalLayoutGroup>();
-            if (parentGroup != null) {
-                parentGroup.ChildDestroyed();
-            }
-        }
-        Destroy(gameObject);
-    }
-
-    private void CheckAndDestroyParent()
-    {
-        if (transform.parent != null && transform.parent.childCount == 1) {
-            Destroy(transform.parent.gameObject);
-        }
-    }
-
-    public void InitializeOrder(List<VegetableType> vegetables)
+    public override void InitializeOrder(List<VegetableType> vegetables)
     {
         this.vegetables = vegetables;
         DisplayOrder();
     }
 
-    public void DisplayOrder()
+    public override void DisplayOrder()
     {
         for (int i = 0; i < vegetables.Count; i++) {
             if (i < vegetableImages.Count) {
-                SetVegetableImage(vegetables[i], vegetableImages[i]);
+                SetVegetableImage(vegetables[i], vegetableImages[i], tomatoSprite, cabbageSprite);
             }
-        }
-    }
-
-    private void SetVegetableImage(VegetableType vegetable, Image vegetableImage)
-    {
-        switch (vegetable)
-        {
-            case VegetableType.Tomato:
-                vegetableImage.sprite = tomatoSprite;
-                break;
-            case VegetableType.Cabbage:
-                vegetableImage.sprite = cabbageSprite;
-                break;
         }
     }
 }
